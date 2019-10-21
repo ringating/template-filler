4aee3c92b58095bc78185b45549b409d79316235
Chris K <34548930+ringating@users.noreply.github.com>
2019-10-21 16:07:39 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+
+
+// ************* stuff to modify ************* //
+
+var templateFilename = "template.txt";
+var outputFilename = "out.txt";
+
+var replace = 
+[
+	{ from: "<NAME>", to: "Shaggy" },
+	{ from: "<COLOR>", to: "Red" },
+];
+
+// ******************************************* //
+
+
+function insertOver(targetStr, subStr, index, nCharsToReplace)
+{
+	return targetStr.slice(0, index).concat(subStr, targetStr.slice(index+nCharsToReplace));
+}
+
+var outputStr = fs.readFileSync(templateFilename, 'utf8');
+
+var lastIndex;
+for(let i = 0; i < replace.length; ++i)
+{
+	while(true)
+	{
+		lastIndex = outputStr.indexOf(replace[i].from);
+		if(lastIndex >= 0)
+		{
+			outputStr = insertOver(outputStr, replace[i].to, lastIndex, replace[i].from.length);
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+fs.writeFileSync(outputFilename, outputStr, 'utf8');
+
+console.log("\nfile written: " + outputFilename + "\n");
